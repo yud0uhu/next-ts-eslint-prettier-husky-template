3c398b5b4b9953ad20203185d0e912c04efe8de6
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-12-26 22:34:20 +0900

    feat: テンプレートのセットアップ

---
+++ src/lib/firebase/firebase.ts
@@ -0,0 +1,15 @@
+import { getApps, getApp, initializeApp, FirebaseApp, FirebaseOptions } from 'firebase/app'
+
+export const config: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_AMESUREMENT_ID,
+}
+
+export const getFirebaseApp = (): FirebaseApp => {
+  return !getApps().length ? initializeApp(config) : getApp()
+}
